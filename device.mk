271464c7d7a8b56afad84f793f928a2750b3a18e
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    ocean: Use Pixel offline charger animation
    Signed-off-by: Arseniy Graur <argraur@gmail.com>
    Change-Id: Ie0d25e37912fffd337731d4c5eb9e9ccf09a2696

--- device.mk
+++ device.mk
@@ -48,6 +48,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/ov12a10_ocean_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/ov12a10_ocean_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/s5k4h7_ocean_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/s5k4h7_ocean_chromatix.xml
 
+# Charger images
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Fingerprint
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
